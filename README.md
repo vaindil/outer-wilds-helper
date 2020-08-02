9434aceec7384e9a8eef9a71bd258e02cad18841
Steven Hall <vaindil@pm.me>
2020-08-02 12:32:39 -0400

    add loop time remaining to pause menu

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # Outer Wilds Helper
 
-This will eventually be a mod for [Outer Wilds](https://store.steampowered.com/app/753640/Outer_Wilds/) containing some tweaks. Uses the [Outer Wilds Mod Loader](https://github.com/amazingalek/owml).
+This is a mod for [Outer Wilds](https://store.steampowered.com/app/753640/Outer_Wilds/) containing some tweaks. Uses the [Outer Wilds Mod Loader](https://github.com/amazingalek/owml).
 
 ## Features
 
 ### Currently Implemented
 
-* None
+* Pause menu displays remaining time in the current loop
 
 ### Currently Planned
 
-* Pause menu displays current time in the loop
+* None yet
---
+++ src/OuterWildsHelper/OuterWildsHelper.cs
@@ -0,0 +1,49 @@
+﻿using OWML.ModHelper;
+using UnityEngine;
+
+namespace OuterWildsHelper
+{
+    public class OuterWildsHelper : ModBehaviour
+    {
+        private string _timeRemainingText;
+        private GUIStyle _timeRemainingStyle;
+
+        private void Start()
+        {
+            ModHelper.Console.WriteLine("Helper: Started");
+
+            ModHelper.Menus.PauseMenu.OnOpen += PauseMenuOpen;
+            ModHelper.Menus.PauseMenu.OnClose += PauseMenuClose;
+        }
+
+        private void PauseMenuOpen()
+        {
+            var part1 = Mathf.Floor(TimeLoop.GetSecondsRemaining() / 60f);
+            var part2 = Mathf.Round(TimeLoop.GetSecondsRemaining() % 60f);
+
+            _timeRemainingText = $"Time Remaining: {part1}:{part2}";
+        }
+
+        private void PauseMenuClose()
+        {
+            _timeRemainingText = null;
+        }
+
+        private void OnGUI()
+        {
+            if (_timeRemainingStyle == null)
+            {
+                _timeRemainingStyle = new GUIStyle(GUI.skin.GetStyle("Label"))
+                {
+                    alignment = TextAnchor.MiddleCenter,
+                    fontSize = 30
+                };
+            }
+
+            if (ModHelper.Menus.PauseMenu.IsOpen && _timeRemainingText != null)
+            {
+                GUI.Label(new Rect((Screen.width / 2) - 150, Screen.height - 200, 300, 100), _timeRemainingText, _timeRemainingStyle);
+            }
+        }
+    }
+}
